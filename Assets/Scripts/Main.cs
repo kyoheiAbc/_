c0fda036826758cad15419162bd28ab32c286f09
kyohei <autumn8november@gmail.com>
2024-03-06 07:24:04 +0900

    Update

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,20 +1,23 @@
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Main
 {
     // private Scene scene = new SceneCharacter();
     // private Input input = new Input();
+    RenderOption renderOption;
     public Main()
     {
         Application.targetFrameRate = 60;
-        new RenderOption();
+        this.renderOption = new RenderOption();
 
     }
     public void Update()
     {
+        this.renderOption.Update();
         // this.scene.Update(this.input.Update());
     }
 }
@@ -65,21 +68,53 @@ public class RenderCharacter : Render
 
 public class RenderOption : Render
 {
+    Slider slider;
+    TextMeshPro textMeshPro;
     public RenderOption()
     {
         this.NewSlider(new Vector2(300, 300), new Vector2(300, 50), new Vector2(0, 0.5f), 25, 75);
 
+        this.crt.AddComponent<GraphicRaycaster>();
+        this.crt.AddComponent<EventSystem>();
+        this.crt.AddComponent<StandaloneInputModule>();
+
     }
-    private Slider NewSlider(Vector2 position, Vector2 size, Vector2 anchor, int min, int max)
+    private void NewSlider(Vector2 position, Vector2 size, Vector2 anchor, int min, int max)
     {
         RectTransform rectTransform = this.NewSprite(position, size, anchor, Resources.Load<Sprite>("Square"), UnityEngine.Color.HSVToRGB(0, 0, 0.25f));
         this.NewSprite(Vector2.zero, Vector2.zero, Vector2.zero, Resources.Load<Sprite>("Square"), UnityEngine.Color.HSVToRGB(0, 0, 0.75f)).SetParent(rectTransform, false);
-        Slider slider = rectTransform.AddComponent<Slider>();
-        slider.fillRect = rectTransform.GetChild(0).GetComponent<RectTransform>();
-        slider.minValue = min;
-        slider.maxValue = max;
-        slider.wholeNumbers = true;
-        return slider;
+        this.slider = rectTransform.AddComponent<Slider>();
+        this.slider.fillRect = rectTransform.GetChild(0).GetComponent<RectTransform>();
+        this.slider.minValue = min;
+        this.slider.maxValue = max;
+        this.slider.wholeNumbers = true;
+
+        TextMeshPro textMeshPro;
+        textMeshPro = this.NewGameObject().AddComponent<TextMeshPro>();
+        textMeshPro.transform.SetParent(rectTransform, false);
+        textMeshPro.GetComponent<RectTransform>().localScale = new Vector3(10, 10, 1);
+        // textMeshPro.GetComponent<RectTransform>().anchorMax = new Vector2(1, 0.5f);
+        // textMeshPro.GetComponent<RectTransform>().anchorMin = new Vector2(1, 0.5f);
+        // textMeshPro.GetComponent<RectTransform>().anchoredPosition = new Vector3(50, 0, 1);
+        textMeshPro.enableWordWrapping = false;
+        textMeshPro.alignment = TextAlignmentOptions.Center;
+        textMeshPro.sortingOrder = 1;
+        this.textMeshPro = textMeshPro;
+
+        textMeshPro = this.NewGameObject().AddComponent<TextMeshPro>();
+        textMeshPro.transform.SetParent(rectTransform, false);
+        textMeshPro.GetComponent<RectTransform>().localScale = new Vector3(10, 10, 1);
+        textMeshPro.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 1);
+        textMeshPro.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 1);
+        textMeshPro.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 50, 1);
+        textMeshPro.enableWordWrapping = false;
+        textMeshPro.alignment = TextAlignmentOptions.Center;
+        textMeshPro.text = "SLIDER SLIDER";
+        textMeshPro.sortingOrder = 1;
+    }
+    public void Update()
+    {
+        this.textMeshPro.text = slider.value.ToString();
     }
 }
 
